1f0668f80e837c662eaab35fd61ef94c791985d1
leonprobst <leonprobst194@gmail.com>
2026-03-10 10:44:53 +0100

    added update, delete and mark-task

--- task-cli.py
+++ task-cli.py
@@ -2,38 +2,101 @@ import sys
 import json
 import datetime
 
-def add(task):
-    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    data = {"id": 0, "description": task, "status": "todo", "createdAt": date, "updatedAt": date}
+date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    #Checks if json exists
+try:
+    with open("tasks.json", "r") as json_file:
+        loaded = json.load(json_file)
+except IOError:
+    pass
+
+def add(task):
+    global date
+    data = {"id": 0, "description": task, "status": "todo", "createdAt": date, "updatedAt": date}
     try:
-        with open("tasks.json", "r") as json_file:
-            loaded = json.load(json_file)
-            data["id"] = loaded.get("todos")[-1].get("id") + 1
-            loaded.get("todos").append(data)
+        data["id"] = loaded.get("todos")[-1].get("id") + 1
+        loaded.get("todos").append(data)
         with open("tasks.json", "w") as json_file:
             json.dump(loaded, json_file, indent=4)
-            return
-    except IOError:
+        return
+    except:
         pass
+    
     #Creates new json if it doesn't exist
     with open("tasks.json", "w") as json_file:
         json.dump({"todos": [data]}, json_file, indent=4)
-        print("test3")
+
+def update(id, task):
+    global date
+    found = False
+
+    for i in loaded.get("todos"):
+        if i.get("id") == int(id):
+            found = True
+            i.update({"description": task, "updatedAt": date})
+            break
+
+    if not found:
+        print("ID not found!")
+        return
+
+    with open("tasks.json", "w") as json_file:
+        json.dump(loaded, json_file, indent=4)
+        return
+
+def delete(id):
+    found = False
+    for i in loaded.get("todos"):
+        if i.get("id") == int(id):
+            found = True
+            loaded.get("todos").remove(i)
+
+    if not found:
+        print("ID not found!")
+        return
+
+    with open("tasks.json", "w") as json_file:
+        json.dump(loaded, json_file, indent=4)
+        return
+
+def mark(id, marktask):
+    global date
+    found = False
+    for i in loaded.get("todos"):
+        if i.get("id") == int(id):
+            found = True
+            i.update({"status": marktask, "updatedAt": date})
+            break
+
+    if not found:
+        print("ID not found!")
+        return
+
+    with open("tasks.json", "w") as json_file:
+        json.dump(loaded, json_file, indent=4)
+        return
+
+def list():
+    return
+
+def list(status):
+    return
+
 
 if __name__ == "__main__":
     operation = sys.argv[1]
     if operation == "add":
         add(sys.argv[2])
     elif operation == "update":
-        print("update")
+        update(sys.argv[2], sys.argv[3])
     elif operation == "delete":
-        print("delete")
+        delete(sys.argv[2])
     elif operation == "mark-in-progress":
-        print("mark in progress")
+        mark(sys.argv[2], "in progress")
     elif operation == "mark-done":
-        print("mark-done")
+        mark(sys.argv[2], "done")
     elif operation == "list":
-        print("list")
+        if(sys.argv[2] != ""):
+            list(sys.argv[2])
+        else: list()
     else: print("Unknown Operation!")
